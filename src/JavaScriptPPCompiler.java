d057e6198da3d8836061e84e3acd656f84d7b811
yss14 <yannickstach@googlemail.com>
2015-01-30 19:08:23 +0100

    Added support for compiling a file list

--- src/JavaScriptPPCompiler.java
+++ src/JavaScriptPPCompiler.java
@@ -12,19 +12,19 @@ import javax.script.ScriptException;
 
 import org.json.JSONObject;
 
-
 public class JavaScriptPPCompiler {
 
 	private static ScriptEngineManager engineManager;
 	private static ScriptEngine engine;
 	private static Boolean hasBeenInit = false;
-	
-	private static void initCompiler() throws MissingDepedencyException, IOException{
+
+	private static void initCompiler() throws MissingDepedencyException,
+			IOException {
 		engineManager = new ScriptEngineManager();
 		engine = engineManager.getEngineByName("nashorn");
-		
+
 		ArrayList<String> jsppFiles = new ArrayList<String>();
-		
+
 		jsppFiles.add("lib/jspp/lib/jspp/lang/es5.js");
 		jsppFiles.add("lib/jspp/src/jsdefs.js");
 		jsppFiles.add("lib/jspp/src/jsparse.js");
@@ -34,8 +34,8 @@ public class JavaScriptPPCompiler {
 		jsppFiles.add("lib/jspp/typesys/strict.js");
 		jsppFiles.add("lib/jspp/src/beautify.js");
 		jsppFiles.add("lib/jspp/wrapper.js");
-		
-		for(String s : jsppFiles){
+
+		for (String s : jsppFiles) {
 			try {
 				System.out.println("Reading dependency " + s + "...");
 				engine.eval(readFileContent(s, false));
@@ -44,56 +44,87 @@ public class JavaScriptPPCompiler {
 			}
 		}
 	}
-	
-	public static CompiledFile compileFile(String sourcePath, String destinationPath) throws IOException, MissingDepedencyException{
-		if(!hasBeenInit){
+
+	public static CompiledFile compileFile(String sourcePath,
+			String destinationPath) throws IOException,
+			MissingDepedencyException {
+		if (!hasBeenInit) {
 			initCompiler();
+			hasBeenInit = true;
 		}
-		
+
+		System.out.println("Compiling " + sourcePath + " ...");
+
 		CompiledFile cf;
-		
+
 		try {
 			String jppString = readFileContent(sourcePath, true);
-			
-			String jsonCode = (String) engine.eval("compileFile(\"" + jppString + "\")");
-			
+
+			String jsonCode = (String) engine.eval("compileFile(\"" + jppString
+					+ "\")");
+
 			JSONObject obj = new JSONObject(jsonCode);
-			
-			if(obj.getString("errors").equals("")){
-				cf = new CompiledFile(sourcePath, destinationPath, obj.getString("compiledCode"), "");
-				
-				if(!destinationPath.equals("")){
-					System.out.println("Writing compiled code to " + destinationPath + "...");
-					BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(destinationPath)));
-					
+
+			if (obj.getString("errors").equals("")) {
+				cf = new CompiledFile(sourcePath, destinationPath,
+						obj.getString("compiledCode"), "");
+
+				if (!destinationPath.equals("")) {
+					System.out.println("Writing compiled code to "
+							+ destinationPath + " ...");
+					BufferedWriter bwr = new BufferedWriter(new FileWriter(
+							new File(destinationPath)));
+
 					bwr.write(obj.getString("compiledCode"));
 					bwr.flush();
 					bwr.close();
 				}
-			}else{
-				cf = new CompiledFile(sourcePath, destinationPath, "", obj.getString("errors"));
+			} else {
+				cf = new CompiledFile(sourcePath, destinationPath, "",
+						obj.getString("errors"));
 			}
 		} catch (ScriptException e) {
 			e.printStackTrace();
-			cf = new CompiledFile(sourcePath, destinationPath, "", "Compiler Error: " + e.toString());
+			cf = new CompiledFile(sourcePath, destinationPath, "",
+					"Compiler Error: " + e.toString());
 		}
-		
+
 		return cf;
 	}
-	
-	public static void main(String[] args) throws MissingDepedencyException{
+
+	public static CompiledFile compileFiles(ArrayList<String> list, String destinationPath) throws IOException, MissingDepedencyException{
+		System.out.println("Merging code...");
 		
-		try {
-			CompiledFile f = compileFile("test/test.jpp", "/Users/yss/Desktop/test.js");
-			System.out.println(f.getCompiledCode());
-		} catch (IOException e) {
-			e.printStackTrace();
+		String code = "";
+		
+		for(String s : list){
+			code += readFileContent(s, true);
 		}
 		
-				
+		String tempPath = "temp/" + getRandomString(10) + "/" + getRandomString(10) + ".jpp";
+		
+		File tmp = new File(tempPath);
+		tmp.getParentFile().mkdirs();
+		tmp.createNewFile();
+		
+		BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(tempPath)));
+		
+		bwr.write(code);
+		bwr.flush();
+		bwr.close();
+		
+		CompiledFile f = compileFile(tempPath, destinationPath);
+		
+		if(f.hasErrors()){
+			return new CompiledFile("", destinationPath, "", f.getErrors());
+		}
+		
+		return new CompiledFile("", destinationPath, code, "");
+		
 	}
-	
-	private static String readFileContent(String path, Boolean minimize) throws IOException{
+
+	private static String readFileContent(String path, Boolean minimize)
+			throws IOException {
 		BufferedReader br;
 		br = new BufferedReader(new FileReader(path));
 		StringBuilder sb = new StringBuilder();
@@ -101,14 +132,51 @@ public class JavaScriptPPCompiler {
 
 		while (line != null) {
 			sb.append(line);
-			if(!minimize){
+			if (!minimize) {
 				sb.append(System.lineSeparator());
-			}else{
+			} else {
 				sb.append(" ");
 			}
 			line = br.readLine();
 		}
 		return sb.toString();
+	}
+
+	private static String getRandomString(int strlen) {
+		final int STRING_LENGTH = strlen;
+		StringBuffer sb = new StringBuffer();
+		
+		for (int i = 0; i < STRING_LENGTH; i++) {
+			sb.append((char) ((int) (Math.random() * 26) + 97));
+		}
+		
+		return sb.toString();
+	}
+
+	public static void main(String[] args) throws MissingDepedencyException {
+
+		try {
+			/*
+			 * CompiledFile f = compileFile("test/ClassA.jpp",
+			 * "/Users/yss/Desktop/ClassA.js"); f =
+			 * compileFile("test/ClassB.jpp", "/Users/yss/Desktop/ClassB.js");
+			 */
+
+			// CompiledFile f = compileFile("test/Combined.jpp",
+			// "/Users/yss/Desktop/Combined.js");
+
+			ArrayList<String> files = new ArrayList<String>();
+
+			files.add("test/ClassA.jpp");
+			files.add("test/ClassB.jpp");
+
+			CompiledFile f = compileFiles(files,
+					"/Users/yss/Desktop/Combined.js");
+
+			// System.out.println(f.getCompiledCode());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
